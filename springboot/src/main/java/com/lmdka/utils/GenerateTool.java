89c7d7d5dcad7dfe4e186b53ab16b97ec0bd5a3b
雷姆的可爱 <2929957153@qq.com>
2024-09-09 00:08:46 +0800

    init

---
+++ springboot/src/main/java/com/lmdka/utils/GenerateTool.java
@@ -0,0 +1,32 @@
+package com.lmdka.utils;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.nio.file.Paths;
+
+public class GenerateTool {
+
+    public static void main(String[] args) {
+        FastAutoGenerator.create("url", "username", "password")
+                .globalConfig(builder -> builder
+                        .author("Baomidou")
+                        .outputDir(Paths.get(System.getProperty("user.dir")) + "/src/main/java")
+                        .commentDate("yyyy-MM-dd")
+                )
+                .packageConfig(builder -> builder
+                        .parent("com.baomidou.mybatisplus")
+                        .entity("entity")
+                        .mapper("mapper")
+                        .service("service")
+                        .serviceImpl("service.impl")
+                        .xml("mapper.xml")
+                )
+                .strategyConfig(builder -> builder
+                        .entityBuilder()
+                        .enableLombok()
+                )
+                .templateEngine(new FreemarkerTemplateEngine())
+                .execute();
+    }
+}
